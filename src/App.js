f7a8d969c0bf51d218b710d42742a4fc00329d7f
Muhammad Saqib <89692631+ssaqi@users.noreply.github.com>
2023-04-12 23:36:10 +0500

    Add files via upload
    Contact

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Contact from './contact/Contact';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min";
+function App() {
+  return (
+<>
+
+<Contact/>
+
+</>
+  );
+}
+
+export default App;
